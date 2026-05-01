19986da35d723fb5978ff1c4aa8b637a61f11f55
frs-chmidt <felixrudolfschmidt@gmail.com>
2026-05-01 15:33:50 +0200

    seperated chat interface logic

---
+++ chat_interface/helpers.py
@@ -0,0 +1,37 @@
+def write_user(self, log: RichLog, text: str) -> None:
+        """
+        Helper function for messages by the user
+
+        Args:
+            log: log object to print to
+            text: text to print
+        """
+
+        log.write("\n[bold #7aa2f7]You[/bold #7aa2f7]")
+        log.write(f"[#c0caf5]{text}[/]\n")
+        log.scroll_end(animate=False)
+
+def write_system(self, log: RichLog, text: str):
+    """
+    Helper function for system messages
+
+    Args:
+        log: log object to print to
+        text: text to print
+    """
+
+    log.write(f"[dim]{text}[/dim]\n")
+    log.scroll_end(animate=False)
+
+def write_assistant(self, log: RichLog, text: str):
+    """
+    Helper function for messages by the llm ("assistant")
+
+    Args:
+        log: log object to print to
+        text: text to print
+    """
+
+    log.write(f"\n[bold #9ece6a]{self.TITLE}[/bold #9ece6a]")
+    log.write(f"[#c0caf5]{text}[/]\n")
+    log.scroll_end(animate=False)
--- main.py
+++ main.py
@@ -18,6 +18,7 @@ from typing_extensions import final
 
 from auth.oauth import resolve_credentials
 from custom_types import CommandHistory, McpConfig, OllamaTool, Mode
+import chat_interface.helpers
 
 MODEL = "qwen3.5:4b"  # TODO: change to other ollama models for testing
 SERVER_SCRIPT = Path(__file__).parent / "mcp_server.py"
@@ -95,43 +96,7 @@ class ChatApp(App):
         self.spinner_frame = 0
         self.spinner_task = None
 
-    def write_user(self, log: RichLog, text: str) -> None:
-        """
-        Helper function for messages by the user
-
-        Args:
-            log: log object to print to
-            text: text to print
-        """
-
-        log.write("\n[bold #7aa2f7]You[/bold #7aa2f7]")
-        log.write(f"[#c0caf5]{text}[/]\n")
-        log.scroll_end(animate=False)
-
-    def write_system(self, log: RichLog, text: str):
-        """
-        Helper function for system messages
-
-        Args:
-            log: log object to print to
-            text: text to print
-        """
-
-        log.write(f"[dim]{text}[/dim]\n")
-        log.scroll_end(animate=False)
-
-    def write_assistant(self, log: RichLog, text: str):
-        """
-        Helper function for messages by the llm ("assistant")
-
-        Args:
-            log: log object to print to
-            text: text to print
-        """
-
-        log.write(f"\n[bold #9ece6a]{self.TITLE}[/bold #9ece6a]")
-        log.write(f"[#c0caf5]{text}[/]\n")
-        log.scroll_end(animate=False)
+    
 
     @override
     def compose(self) -> ComposeResult:
