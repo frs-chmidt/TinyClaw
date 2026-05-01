8de4a960a5e030674d64395fc7907b2addc0eaaa
stfx7777 <stefanpoell.office@gmail.com>
2026-05-01 17:03:39 +0200

    .

--- plugins/time.py
+++ plugins/time.py
@@ -11,10 +11,6 @@ def get_current_date_time() -> str:
     """
     Get the current system time and date (datetime) in isoformat
 
-    Use this when:
-    - user asks for time/date
-    - user says "now", "current", "today"
-
     Returns:
     - string timestamp in isoformat
     """
